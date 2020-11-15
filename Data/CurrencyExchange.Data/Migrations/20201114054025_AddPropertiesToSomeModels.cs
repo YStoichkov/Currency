7db11609e5e050ae62f6242612ea77d07b70c9d8
YankoStoichkov <ystoichkov@gmail.com>
2020-11-15 18:44:15 +0000

    Minor changes to View Models

--- Data/CurrencyExchange.Data/Migrations/20201114054025_AddPropertiesToSomeModels.cs
+++ Data/CurrencyExchange.Data/Migrations/20201114054025_AddPropertiesToSomeModels.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-
-namespace CurrencyExchange.Data.Migrations
+﻿namespace CurrencyExchange.Data.Migrations
 {
+    using Microsoft.EntityFrameworkCore.Migrations;
+
     public partial class AddPropertiesToSomeModels : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
